715313ebcdf896e137b2425c1bec5b0917ac0d73
Glory Ngum Foncha <gloriane7767@gmail.com>
2025-12-03 10:51:41 +0100

    add logic for TemperatureConverter

--- src/main/java/com/gloriane/ConverterApp.java
+++ src/main/java/com/gloriane/ConverterApp.java
@@ -38,6 +38,7 @@ public class ConverterApp {
         System.out.println("\n=== Currency Converter Menu ===");
         System.out.println("1. Convert SEK to USD");
         System.out.println("2. Convert USD to SEK");
+
         System.out.println("3. Convert SEK to Euro");
         System.out.println("4. Convert Euro to SEK");
         System.out.println("5. Exit");
--- src/main/java/com/gloriane/TemperatureConverter.java
+++ src/main/java/com/gloriane/TemperatureConverter.java
@@ -1,4 +1,31 @@
 package com.gloriane;
 
+import java.util.Scanner;
+
 public class TemperatureConverter {
+    public void convert() {
+        System.out.println("Temperature Converter Selected.");
+        Scanner scanner = new Scanner(System.in);
+
+        // Implementation of temperature conversion logic goes here
+        System.out.println("Enter Temperature: ");
+        double temperature = scanner.nextDouble();
+        System.out.println("Convert to: ");
+        System.out.println("1. Celsius to Fahrenheit");
+        System.out.println("2. Fahrenheit to Celsius");
+        int choice = scanner.nextInt();
+        double result;
+        switch (choice) {
+            case 1:
+                result = (temperature * 9/5) + 32;
+                System.out.printf("%.2f Celsius = %.2f Fahrenheit%n", temperature, result);
+                break;
+            case 2:
+                result = (temperature - 32) * 5/9;
+                System.out.printf("%.2f Fahrenheit = %.2f Celsius%n", temperature, result);
+                break;
+            default:
+                System.out.println("Invalid choice. Please select a valid option.");
+        }
+    }
 }
